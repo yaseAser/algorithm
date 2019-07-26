eca4325e9210b2361baf656735fd2fcdcfa7f0c3
aser <strongaser666@gmail.com>
2019-07-26 11:12:10 +0700

    第三道题：整数反转

---
+++ 03-整数反转.java
@@ -0,0 +1,34 @@
+/**
+    给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+    注意:
+
+    假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−231,  231 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。
+
+    来源：力扣（LeetCode）
+    链接：https://leetcode-cn.com/problems/reverse-integer
+    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+class Solution {
+    public int reverse(int x) {
+        long temp = 0;
+       
+        while(x != 0){
+            int pop = x % 10;
+            temp = temp * 10 + pop;
+            
+            if(temp > Integer.MAX_VALUE || temp < Integer.MIN_VALUE){
+                return 0;
+            }
+            x /= 10;
+        }
+        return (int)temp;
+    }
+
+}
+
+/**
+    1032 / 1032 个通过测试用例
+    状态：通过
+    执行用时：6 ms
+*/
