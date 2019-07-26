df96029542951b157f0a9e467c45df9903e95a1a
aser <strongaser666@gmail.com>
2019-07-26 14:50:15 +0700

    回文数

---
+++ 04-回文数.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+	判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+**/
+
+class Solution {
+
+    /**
+     * @param Integer $x
+     * @return Boolean
+     */
+    function isPalindrome($x) {
+        $y = (string)$x;
+        $len = strlen($y);
+        $result = true;
+
+        $i = 0;
+        while ($i < ($len - $i - 1)) {
+            if($y[$i] != $y[$len - $i - 1]){
+                $result = false;
+            }
+            $i ++;
+        }
+        return $result;
+    }
+}
